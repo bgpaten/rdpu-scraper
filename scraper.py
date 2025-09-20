a449ee5971c5c4488d2861263fa2c41b9d1e7a7c
bgpaten <ahyarpattani@gmail.com>
2025-09-20 11:37:20 +0700

    upsate not insert done

--- scraper.py
+++ scraper.py
@@ -8,7 +8,8 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # GOLD
-GOLD_ID = 1  # id row di tabel prices untuk Gold
+PRICE_ID_GOLD = 1  # id row di tabel prices
+ASSET_ID_GOLD = 4  # id Gold di tabel assets
 URL = "https://api.exchangerate.host/convert?access_key=d4fe6549a0d4c4c4a0e919dcd6698dd7&from=XAU&to=IDR&amount=1"
 
 try:
@@ -24,11 +25,12 @@ try:
     price_per_gram = xau_to_idr / gram_per_xau
 
     record = {
+        "asset_id": ASSET_ID_GOLD,
         "price": round(price_per_gram, 2),
         "price_time": datetime.utcnow().isoformat(),
     }
 
-    response = supabase.table("prices").update(record).eq("id", GOLD_ID).execute()
+    response = supabase.table("prices").update(record).eq("id", PRICE_ID_GOLD).execute()
     print("✅ Gold price berhasil diupdate:", response)
 
 except Exception as e:
@@ -36,7 +38,8 @@ except Exception as e:
 
 
 # BTC
-BTC_ID = 3  # id row di tabel prices untuk BTC
+PRICE_ID_BTC = 3  # id row di tabel prices
+ASSET_ID_BTC = 5  # id BTC di tabel assets
 BTC_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=idr"
 
 try:
@@ -50,11 +53,12 @@ try:
     btc_to_idr = data["bitcoin"]["idr"]
 
     record = {
+        "asset_id": ASSET_ID_BTC,
         "price": btc_to_idr,
         "price_time": datetime.utcnow().isoformat(),
     }
 
-    response = supabase.table("prices").update(record).eq("id", BTC_ID).execute()
+    response = supabase.table("prices").update(record).eq("id", PRICE_ID_BTC).execute()
     print("✅ BTC price berhasil diupdate:", response)
 
 except Exception as e:
